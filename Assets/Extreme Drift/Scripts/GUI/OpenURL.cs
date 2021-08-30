d4de436d3a37780cc7a9d1521a633528873cf6d1
LinhBKU <linh.huynh.dev.99@gmail.com>
2021-08-30 09:33:32 +0700

    car v2 init

---
+++ Assets/Extreme Drift/Scripts/GUI/OpenURL.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenURL : MonoBehaviour {
+
+	public void OpenTab (string URL) {
+		Debug.Log(URL);
+        Application.OpenURL(URL);
+	}
+
+}
